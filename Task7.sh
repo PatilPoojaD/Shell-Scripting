ae9bbc4b7b72092fe9836372cb12dfda22dab75d
PatilPoojaD <80782935+PatilPoojaD@users.noreply.github.com>
2021-12-06 23:52:27 -0600

    Create Task7.sh

---
+++ Task7.sh
@@ -0,0 +1,13 @@
+ grep -w 'the' Task7.txt
+ 
+ 
+ 
+Ouput
+
+
+But as the riper should by time decease,
+Thou that art now the world's fresh ornament,
+And only herald to the gaudy spring,
+Pity the world, or else this glutton be,
+To eat the world's due, by the grave and thee.
+Where all the treasure of thy lusty days;
